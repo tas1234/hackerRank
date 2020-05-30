4ecea3154530c202a83aae3e0467e72e18b3abf9
tas1234 <tasnim.sakka@gmail.com>
2020-05-30 20:19:25 +0100

    Add files via upload

---
+++ java Subarray.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int size = scanner.nextInt();
+        int[] array= new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+    
+        int count =0;
+        
+        for (int i=0; i<size; i++){
+            int sum =array[i];
+            if (sum < 0){
+                count++;
+            }
+
+            for (int j=i+1; j<size; j++){
+                sum+= array[j];
+                if (sum<0) {
+                    count++;
+                }
+            
+            }
+        }
+        scanner.close();
+        System.out.println(count); 
+        
+
+    }
+}
